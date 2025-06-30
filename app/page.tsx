189bb84090ed2be0382a810d6f454c9e3daa6cba
anushree200 <aanuushree@gmail.com>
2025-06-30 15:23:50 +0530

    added next component in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-import React from 'react';
+import HomePage from './home/Homepage';
 
-export default function HomePage() {
+export default function Page() {
   return (
-    <div className="w-full h-[90vh] bg-gray-100 relative overflow-hidden">
-      <video 
-        className="w-full h-[90vh] object-cover brightness-65"
-        src="/glitch_promo.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center">
-        <img src="/logo-nobg.png" alt="Glitch Logo" className="h-16 mr-4" />
-        <span className="text-6xl font-bold text-primary" style={{ textShadow: "0 0 30px #00ff00, 0 0 30px #00ff00, 0 0 0 #00ff00" }}>
-          GLITCH
-        </span>
-      </div>
+    <div>
+      <HomePage />
     </div>
   );
 }
